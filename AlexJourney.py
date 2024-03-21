aba262e7e2e5b7c15048ec898c76af679f82894f
Arshia <arshi829495@gmail.com>
2024-03-21 20:34:45 +0330

    added-keyword-counter

--- AlexJourney.py
+++ AlexJourney.py
@@ -1,9 +1,9 @@
 import keyword
 
 
-def decrypt_clue(text):
+def decrypt_clue(text: str):
     refrence = keyword.kwlist
-    result = [i for i in refrence if i in text]
+    result = [(i, text.count(i)) for i in refrence if i in text]
     return result
 
 file = open("mysterious.txt", "r")
