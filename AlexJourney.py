6bff19c7549387c8ae484211fa73ecc1178a57a4
Arshia <arshi829495@gmail.com>
2024-03-29 14:14:56 +0330

    compelete-mission-3

--- AlexJourney.py
+++ AlexJourney.py
@@ -1,18 +1,22 @@
 import random
-import datetime
+import time
+import string
 
 
 def binary(decimal):
+    deci = decimal
     i = 0
     q = 0
     result = 0
-    while decimal>=2:
-        result += 10**i * (decimal%2)
-        q = decimal // 2
-        decimal //= 2
+    while deci >= 2 or decimal == 1:
+        result += 10**i * (deci % 2)
+        q = deci // 2
+        deci //= 2
         i += 1
+        decimal = 0
     result += q*10**i
     return result
+
 
 def calculate_magic_numbers(start, end):
     result = [random.randint(start, end) for i in range(10)]
@@ -21,9 +25,39 @@ def calculate_magic_numbers(start, end):
 
 
 def exam_numbers():
-    decimal = random.randint(0, 15)
-    print(decimal)
-    result = binary(decimal) 
+    corrects = 0
+    mistakes = 0
+    start = time.time()
+    while time.time() - start < 20:
+        decimal = random.randint(0, 15)
+        result = binary(decimal)
+        userInput = ""
+        try:
+            userInput = int(input(f"{decimal} in binary is : "))
+        except:
+            pass
+        if result == userInput:
+            corrects += 1
+        else:
+            mistakes += 1
+    return f"{corrects} corrects \n{mistakes} wrongs"
+
+
+# print(exam_numbers())
+
+def check_pass(users_list):
+    result = []
+    for i in users_list:
+        if (
+            list(filter(lambda x: x.isupper(), i[1])) and
+            list(filter(lambda x: x.islower(), i[1])) and
+            list(filter(lambda x: x in string.punctuation, i[1])) and 
+            len(i[1])>8 
+            ):
+            result.append(i[0])
     return result
 
-print(exam_numbers())
+# lst = [("a" , "Ali12*2333") , ('b', 'Aswer&&&') , ('c', "hassan#2@"), ('re',"HAAAAA!@#"), ('e' , "Asgh@")]
+# check = check_pass(lst)
+# print(check)
+            
