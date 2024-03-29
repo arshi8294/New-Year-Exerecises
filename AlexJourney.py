0de1d68c1c5729358cbe744f23e6d8a7375b1177
Arshia <arshi829495@gmail.com>
2024-03-29 10:08:51 +0330

    fix-issues

--- AlexJourney.py
+++ AlexJourney.py
@@ -1,11 +1,11 @@
 import keyword
 
 
-def decrypt_clue(text: str):
+def decrypt_clue(file):
+
+    text = file.read()
     refrence = keyword.kwlist
     result = [(i, text.count(i)) for i in refrence if i in text]
     return result
 
-file = open("mysterious.txt", "r")
-text = file.read()
-print(decrypt_clue(text))
+print(decrypt_clue(file = open("mysterious.txt", "r")))
